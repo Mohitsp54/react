e82dcd166341936794f9bf91d374399ebeaf8848
Mohitsp54 <mohitsp54@gmail.com>
2025-01-13 14:10:11 +0530

    changed

--- react-mohit/src/App.jsx
+++ react-mohit/src/App.jsx
@@ -24,15 +24,41 @@ export const App = ()=>{
 };
 
 const NetflixSeries = () =>{
+  const name = "Queen of Tears";
+  const rating = 8.5;
+  const summary = "Nice Movie";
+  const returnGenre = () =>{
+    const genre = "Romcom";
+    return genre;
+  };
+  let age =14;
+  // if(age < 18){
+  //   return (
+  //     <div>
+  //     <div>
+  //       <img src="netflix.jpg" alt ="heroSection.svg" 
+  //       width="40%" height="40%"/>
+  //     </div>
+  //     <h2>Name: {name}</h2>
+  //     <h3>Rating:{rating}</h3>
+  //     <p>summary:{summary}</p>
+  //     <p>Genre:{returnGenre()}</p>
+  //     <button>Not Available</button>
+  //   </div> 
+  //   )
+  // }
+
   return (
     <div>
       <div>
         <img src="netflix.jpg" alt ="heroSection.svg" 
         width="40%" height="40%"/>
       </div>
-      <h2>Name: Queen of Tears</h2>
-      <h3>Rating:8.5</h3>
-      <p>summary:Nice Movie</p>
+      <h2>Name: {name}</h2>
+      <h3>Rating:{rating}</h3>
+      <p>summary:{summary}</p>
+      <p>Genre:{returnGenre()}</p>
+      <button>{age >= 18?"Watch Now":"Not Available"}</button>
     </div>);
 };
 
